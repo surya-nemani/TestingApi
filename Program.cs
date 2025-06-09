84610176abfb4a01fb2bbd208763ae4221dd0991
Surya Nemani <surya.nemani@fpt.com>
2025-06-09 16:35:07 -0400

    added new message

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World Roxie team!");
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
